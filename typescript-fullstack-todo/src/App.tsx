46bf814a8d6a36f50680f2c98cb1cd3cd7787f24
Akash-298 <akash.c.c298@gmail.com>
2023-08-02 22:13:21 +0530

    material ui installation and custom theme and more setup

--- typescript-fullstack-todo/src/App.tsx
+++ typescript-fullstack-todo/src/App.tsx
@@ -1,6 +1,13 @@
-import React,{FC,ReactElement} from 'react';
+import { customTheme } from './theme/customTheme';
 
+import React,{FC,ReactElement} from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 const App:FC = ():ReactElement =>{
-  return <h1>Hello world</h1>
-}
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline></CssBaseline>
+       <h1>Hello world</h1>
+    </ThemeProvider>
+ 
+)}
 export default App;
