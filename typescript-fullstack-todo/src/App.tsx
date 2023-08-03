bb21a5e55aa5112c09731f007b8e790e425d0386
Akash-298 <akash.c.c298@gmail.com>
2023-08-03 13:33:53 +0530

    working on the form part of frontend

--- typescript-fullstack-todo/src/App.tsx
+++ typescript-fullstack-todo/src/App.tsx
@@ -2,11 +2,14 @@ import { customTheme } from './theme/customTheme';
 
 import React,{FC,ReactElement} from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Dashboard } from './pages/dashboard/dashboard';
 const App:FC = ():ReactElement =>{
   return (
     <ThemeProvider theme={customTheme}>
-      <CssBaseline></CssBaseline>
-       <h1>Hello world</h1>
+      <CssBaseline>
+      <Dashboard />
+      </CssBaseline>
+       {/* <h1>Hello world</h1> */}
     </ThemeProvider>
  
 )}
